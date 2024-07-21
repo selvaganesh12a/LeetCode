50ad55922b2bba844317cfabcddca748a78ef04e
selvaganesh12a <135496583+selvaganesh12a@users.noreply.github.com>
2024-07-21 10:43:49 +0530

    Add files via upload

---
+++ buildArray.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class buildArray {
+    public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i]=ans[nums[nums[i]]];
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int[] num = new int[in.nextInt()];
+        System.out.println(Arrays.toString(buildArray(num)));
+    }
+}
