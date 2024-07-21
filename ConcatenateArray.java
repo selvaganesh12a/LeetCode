838ee963bb8b9915a53c47c7e2fab0c745728693
selvaganesh12a <135496583+selvaganesh12a@users.noreply.github.com>
2024-07-21 10:07:25 +0530

    Add files via upload

---
+++ ConcatenateArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConcatenateArray {
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            ans[i + i] = nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main (String[]args){
+        Scanner in = new Scanner(System.in);
+        int[] num = new int[in.nextInt()];
+        System.out.println(Arrays.toString(getConcatenation(num)));
+    }
+}
