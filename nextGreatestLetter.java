3c6fcf914b29cee64a676ff7549dd655783d3a14
selvaganesh12a <135496583+selvaganesh12a@users.noreply.github.com>
2024-07-28 13:50:28 +0530

    Add files via upload

---
+++ nextGreatestLetter.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class nextGreatestLetter {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        for (int i = 0; i < letters.length; i++) {
+            if(letters[i]>target){
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("letters = ");
+        char[] letters = new char[in.nextInt()];
+        System.out.println(", target = ");
+        char target = in.next().charAt(0);
+        System.out.println(nextGreatestLetter(letters,target));
+    }
+}
