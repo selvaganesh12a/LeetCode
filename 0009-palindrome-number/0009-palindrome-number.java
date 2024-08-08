46d493896b2db5f490d60ed16eed6cae069093f2
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2024-08-08 16:25:04 +0530

    Time: 4 ms (100%), Space: 43.9 MB (42.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        boolean result = false;
+      //  boolean result = false;
         int rev = 0;
         int rem = 0;
         int temp = x;
 
         if(x<0){
-            return false;
+           return false;
+        //    // -121
+        //    x = x*(-1);
         }
-
         while(temp != 0){
             rem = temp % 10;
             temp /= 10;
