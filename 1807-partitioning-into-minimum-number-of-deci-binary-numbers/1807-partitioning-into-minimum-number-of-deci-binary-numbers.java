86ae49550ebe9cadf9736008763335d5dee3899f
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-01-04 20:49:57 +0530

    Time: 3 ms (100%), Space: 45.6 MB (14.78%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,11 +1,10 @@
 class Solution {
     public int minPartitions(String n) {
         int max = 0;
-        int num = 0;
-        for (int i = 0; i <= n.length() - 1; i++) {
-            num = n.charAt(i) - 48;
-            if(num > max){
-                max = num;
+        for (int i = 9; i >= 0; i--) {
+            if(n.contains(String.valueOf(i))){
+                max = i;
+                break;
             }
         }
         return max;
