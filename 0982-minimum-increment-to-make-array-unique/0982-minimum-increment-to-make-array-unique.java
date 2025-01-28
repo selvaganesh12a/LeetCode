49a27a958ce993b5d20b63c05748cd4ed53f6512
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-01-28 20:47:59 +0530

    Time: 39 ms (22.04%), Space: 59.4 MB (52.61%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -4,7 +4,7 @@ class Solution {
         int count = 0;
         for (int i = 1; i < nums.length; i++) {
             if(nums[i] <= nums[i-1]){
-                count = count + (nums[i-1] - nums[i] +1);
+                count += (nums[i-1] - nums[i] +1);
                 nums[i] += (nums[i-1] - nums[i]) + 1;
             }
         }
