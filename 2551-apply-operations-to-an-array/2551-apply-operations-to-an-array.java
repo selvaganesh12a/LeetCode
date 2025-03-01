b3058681f27c5f0d98f94e24ecbb91cf91822666
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-03-01 21:59:27 +0530

    Time: 2 ms (8.49%), Space: 44.7 MB (5.12%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -11,7 +11,7 @@ class Solution {
                 temp[i+1] = 0;
             }
         }
-        System.out.println(Arrays.toString(temp));
+        
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < n; i++){
             if(temp[i] > 0){
