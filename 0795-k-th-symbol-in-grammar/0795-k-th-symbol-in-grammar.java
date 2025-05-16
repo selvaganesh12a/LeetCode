0c294363b7767c30629a8b9dedafbde19a55d599
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-05-16 16:04:12 +0530

    Time: 0 ms (100%), Space: 40.3 MB (66.67%) - LeetHub

---
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int kthGrammar(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+
+        if (k % 2 == 1) {
+            return kthGrammar(n - 1, (k + 1) / 2);
+        } else {
+            return 1 - kthGrammar(n - 1, k / 2);
+        }
+    }
+}
