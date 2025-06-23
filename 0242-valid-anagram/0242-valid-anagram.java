a241c615817214ca3cd7b13c0a1596852043fee5
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-06-23 10:08:33 +0530

    Time: 4 ms (77.82%), Space: 44.7 MB (43.03%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(s.length() == t.length()){
+            for(int i = 0; i < s.length(); i++){
+                if(a[i] != b[i]) return false;
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
