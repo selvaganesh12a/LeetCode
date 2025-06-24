f75127d97acd13b9141f5fbce5dde81cb2779d5e
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-06-24 12:26:47 +0530

    Time: 4 ms (99.6%), Space: 42.7 MB (40.46%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,23 +1,23 @@
 class Solution {
-    public long pickGifts(int[] gifts, int k) {
+    static{
+        for(int i=0;i<500;i++){
+            pickGifts(new int[]{},0);
+        }
+    }
+    public static long pickGifts(int[] gifts, int k) {
         long ans = 0;
         int n = gifts.length;
+        Queue<Integer> pq = new PriorityQueue<>((a,b) ->Long.compare(b,a));
+        for(int i = 0; i < n; i++){
+            pq.add(gifts[i]);
+        }
         while(k > 0){
-            Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-            for(int i = 0; i < n; i++){
-                pq.add(gifts[i]);
-            }
             int del = pq.poll();
             int val = (int)(Math.floor(Math.sqrt(del)));
-            for(int i = 0; i < n; i++){
-                if(del == gifts[i]) {
-                    gifts[i] = val;
-                    break;
-                }
-            }
+            pq.add(val);
             k--;
         }
-        for(int i: gifts) ans += i;
+        for(int i: pq) ans += i;
         return ans;
     }
 }
